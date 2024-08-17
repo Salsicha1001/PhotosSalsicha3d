d2266608e64f7f186a2f8fc8217399e80e8a394e
Salsicha1001 <felumais14@gmail.com>
2024-08-17 00:35:00 -0300

    teste subida

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-
+const express = require('express');
+const app = express();
+const port = 3000;
 function readDirRecursive(dirPath) {
     const result = { name: path.basename(dirPath), type: 'directory', children: [] };
 
@@ -18,8 +20,16 @@ function readDirRecursive(dirPath) {
 
     return result;
 }
+ 
+app.get('/api/files', (req, res) => {
+    const projectDir = path.resolve(__dirname); // Ajuste conforme necessário
+    const directoryStructure = readDirRecursive(projectDir);
+    res.json(directoryStructure);
+});
 
-const projectDir = path.resolve(__dirname);
-const directoryStructure = readDirRecursive(projectDir);
+// Servir arquivos estáticos, como HTML e CSS
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(JSON.stringify(directoryStructure, null, 2));
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
